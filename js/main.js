f5998a4d5fedfcdc8f79165bbdd523fc51428c55
moazzam shah <moazzam.shah106@gmail.com>
2020-10-22 00:28:05 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// SELECT DOM ITEMS
+
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+
+const navItems = document.querySelectorAll('.nav-item');
+
+
+// SET Initial state ióf menu
+
+let showMenu = false;
+
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+    // setting menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    // setting menu state
+    showMenu = false;
+  }
+}
